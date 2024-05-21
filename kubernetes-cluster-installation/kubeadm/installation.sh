245791e16e4d7d9933dcd6b1b7508479d6593559
adminturneddevops <mlevan1992@gmail.com>
2024-05-21 13:22:53 -0400

    install

--- kubernetes-cluster-installation/kubeadm/installation.sh
+++ kubernetes-cluster-installation/kubeadm/installation.sh
@@ -5,7 +5,7 @@ sudo apt update -y
 sudo apt-get install -y apt-transport-https curl
 
 # Add Kubernetes Signing Key
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 # Add Software Repositories
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
