8bb60272094c7cae30b6cbca33f6ca39589bd03a
adminturneddevops <mlevan1992@gmail.com>
2024-06-29 18:19:57 -0400

    kubevirt instructions

--- virtualized-kubernetes/install.sh
+++ virtualized-kubernetes/install.sh
@@ -1,16 +1,19 @@
+## To Add Specific Version
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-operator.yaml
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-cr.yaml
+
+
+## To Add The Latest Version
 export VERSION=$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases | grep tag_name | grep -v -- '-rc' | sort -r | head -1 | awk -F': ' '{print $2}' | sed 's/,//' | xargs)
 
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-operator.yaml
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$VERSION/kubevirt-cr.yaml
 
 
-## To Add Specific Version
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-operator.yaml
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-cr.yaml
-
 ## To Delete
 kubectl delete -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-operator.yaml
 kubectl delete -f https://github.com/kubevirt/kubevirt/releases/download/v1.2.0/kubevirt-cr.yaml
+
 
 ## Delete All CRDs
 MATCH_STRING="kubevirt"
